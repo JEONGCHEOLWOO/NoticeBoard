794f495cb63ee9561e8c7eb618770970223317d4
JEONG Cheolwoo <tonyprince1228@gmail.com>
2026-01-27 19:49:43 +0900

    2026.01.27
    Webconfig - Interceptor 설정 공부 및 주석 추가, HandlerInterceptor 인터페이스 공부 및 주석 추가.

--- src/main/java/com/example/NoticeBoard/Interceptor/LogInterceptor.java
+++ src/main/java/com/example/NoticeBoard/Interceptor/LogInterceptor.java
@@ -8,12 +8,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
-    // Controller 메소드 호출 전 
+    // HandlerInterceptor 인터페이스에는 preHandle(), postHandle(), afterCompletion() 총 3가지 메소드가 있다.
+    // preHandle() 메소드는 Controller 메소드 호출 전에 실행 되고,
+    // postHandle() 메소드는 Controller 메소드 호출 이후, View 렌더링 전(Dispatcher Servlet이 화면을 처리하기 전)에 실행 되고,
+    // afterCompletion() 메소드는 Dispatcher Servlet 의 화면 처리(Veiw)가 완료된 상태에서 실행된다.   
+    // Dispatcher Servlet 은 모든 요청의 출입구, Spring MVC의 프론트 컨트롤러의 역활
+
     @Override
-    public boolean preHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler){
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         log.info("REQUSET [{}] {}", request.getMethod(), request.getRequestURI());
         return true;
     }
--- src/main/java/com/example/NoticeBoard/config/WebConfig.java
+++ src/main/java/com/example/NoticeBoard/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     // CORS(Cross-Origin Resource Sharing, 교차 출처 리소스 공유) 설정 -> Front에서 사용하는 도메인과 Back에서 사용하는 도메인의 주소가 다를 떄 요청과 응답을 받을 수 있도록 허용하는 설정
     // http://localhost:3000/post/1 -> http: 는 프로토콜(protocol), localhost 는 도메인(hostname), 3000 은 포트(port), post/1 은 경로(pathname)
-    // 출처(Origin) - 프로토콜 + 도메인 + 포트를 말함. (http://localhost:3000
+    // 출처(Origin) - 프로토콜 + 도메인 + 포트를 말함. (http://localhost:3000)
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**") // 어떤 경로에 적용할지
@@ -24,11 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600); // preflight 캐시 시간 (1시간)
     }
 
+    // Interceptor 설정 -> 컨트롤러를 호출하기 전과 후에 요청이나 응답을 가로채 미리 정의된 로직(인증, 로깅 등)을 적용하는 기능, API 호출에 대한 로깅이나 감사를 진행.
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(new LogInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns(
+                .addPathPatterns("/**")     // 적용할 경로 (모든 경로)
+                .excludePathPatterns(       // 제외할 경로
                         "/auth/**",
                         "/mail/**",
                         "/error/**",
